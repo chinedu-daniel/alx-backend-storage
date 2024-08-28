13a2b12d7915b5c6efbc9e71e652c075efef4fb6
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-08-28 13:02:15 +0100

    exercise.py

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Writing strings to Redis
+"""
+import Redis
+import uuid
+
+
+class Cache():
+    """
+    Create a Cache class
+    """
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb
+
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        store a data argument and returns a string
+        """
+        uuid_num = str(uuid.uuid4())
+        self.redis.set(uuid_num, data)
+
+        return uuid_num

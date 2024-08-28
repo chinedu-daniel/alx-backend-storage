51d41ef82fa3a0ca28bee1bcc410c59a6765596c
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-08-28 20:05:34 +0100

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,6 +8,58 @@ from typing import Any, Union, Optional, Callable
 from functools import wraps
 
 
+def count_calls(method: Callable) -> Callable:
+    """Decorator that counts method calls and increments a Redis counter"""
+
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapperfunction"""
+
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wwrapper
+
+
+def replay(method: Callable) -> str:
+    """replay function"""
+
+    m_key = method.__qualname__
+    inputs = method.__qualname__ + ":inputs"
+    outputs = method.__qualname__ + ":outputs"
+
+    redis = method.__self__._redis
+    print(redis)
+    count = redis.get(m_key).decode("utf-8")
+    print('{} was called {} times'.format(m_key, count))
+    allinput = redis.lrange(inputs, 0, -1)
+    alloutput = redis.lrange(outputs, 0, -1)
+    allData = list(zip(alloutput, allinput))
+    for k, v in allData:
+        key = k.decode("utf-8")
+        v = v.decode("utf-8")
+        print(f"{m_key}(*{v}) -> {key}")
+
+
+def call_history(method: Callable) -> Callable:
+    """Get the current inputs and outputs"""
+    key = method.__qualname__
+    inputs = method.__qualname__ + ":inputs"
+    outputs = method.__qualname__ + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> Callable:
+        """Get the current inputs and outputs"""
+
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwargs)
+        self._redis.rpush(outputs, data)
+        return data
+    return wrapper
+
+
 class Cache:
     """
     Create a Cache class
